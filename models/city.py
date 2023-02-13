7c2d5f43322b54d503df90a9dfc7d46d9767b4fd
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-02-13 09:36:52 +0000

    Create City class

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for City"""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """class to manage City object"""
+
+    state_id = ""
+    name = ""
