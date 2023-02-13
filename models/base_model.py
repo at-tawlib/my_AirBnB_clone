14bdd5a0377dff8ddfc64730f81ed382ac26f48e
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-02-13 08:49:04 +0000

    Add kwargs and args to init of base_model

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,20 @@ from datetime import datetime
 class BaseModel():
     """Base class for all other classess"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """constructor function"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = self.created_at
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     
     def __str__(self):
